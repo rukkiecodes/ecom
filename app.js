f6c2958025cf3c293e45222865e8f183125bbe5f
rukkiecodes <rukkiecodes@gmail.com>
2023-09-18 11:39:20 +0100

    get profile

--- app.js
+++ app.js
@@ -39,6 +39,7 @@ if (process.env.NODE_ENV === "development") app.use(morgan("dev"))
 app.use('/auth', [
   require('./routes/auth/signup'),
   require('./routes/auth/signin'),
+  require('./routes/auth/getProfile'),
 ])
 
 
