bf5d6e5de83582783082db5a958c9b02672221ba
rukkiecodes <rukkiecodes@gmail.com>
2023-09-25 10:26:44 +0100

    update avatar

--- routes/auth/updateAvatar.js
+++ routes/auth/updateAvatar.js
@@ -9,6 +9,8 @@ const User = require("../../models/user")
 router.post("/avatar", upload.single("avatar"), async (req, res) => {
   const { email } = req.body
 
+  let userProfile = await User.findOne({ email })
+
   try {
     // Upload image to cloudinary
     const result = await cloudinary.uploader.upload(req.file.path, {
@@ -16,7 +18,13 @@ router.post("/avatar", upload.single("avatar"), async (req, res) => {
     })
 
     let user
-    user = await User.updateOne({ email }, { $set: { avatar: result.secure_url } })
+    user = await User.updateOne({ email }, {
+      $set: {
+        avatar: result.secure_url,
+        name: userProfile.name,
+        phone: userProfile.phone
+      }
+    })
 
     res.status(200).json({
       message: "Avatar updated",
